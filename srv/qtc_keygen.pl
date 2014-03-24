5cf6dd539af21954e9250489168379c0a71b5de4
Hans Freitag <zem.nospam.20@fnordpol.de>
2014-03-24 13:57:35 +0100

    secured key generation

--- srv/qtc_keygen.pl
+++ srv/qtc_keygen.pl
@@ -23,9 +23,13 @@ my $pubkey=qtc::msg->new(
 ); 
 
 # selfsign message first
+#print STDERR length($pubkey->checksum)."\n";
+#print STDERR length($rsa->sign($pubkey->checksum.$pubkey->checksum.$pubkey->checksum.$pubkey->checksum.$pubkey->checksum))."\n";
 $pubkey->signature(encode_base64($rsa->sign($pubkey->checksum)), $key_id); 
 
 my $path=$ENV{HOME}."/.qtc_private";
+
+if ( -e "$path/rsa_$call.key" ) { die "ther is already a key, it may be a bad idea to write a new one\n"; }
 print STDERR "Writing Keys to $path\n"; 
 
 $pubkey->ensure_path($path); 
