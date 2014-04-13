fb3c2f856cd074f4ad976c1f725686fc748e081f
Hans Freitag <zem.nospam.20@fnordpol.de>
2014-04-13 14:34:44 +0200

    read call,
    but discovered that i must move some
    of the call and telegram text reducing methods.
    to qtc::misc

--- src/qtc/WebApp.pm
+++ src/qtc/WebApp.pm
@@ -1,6 +1,8 @@
 package qtc::WebApp; 
 
 use base 'CGI::Application'; 
+use qtc::query; 
+use qtc::publish; 
 
 sub setup {
 	my $obj = shift;
@@ -9,11 +11,26 @@ sub setup {
 	$obj->run_modes(
 		'ask_call' => 'mode_ask_call',
 	);
+	if ( ! $obj->{qtc}->{path} ) { $obj->{qtc}->{path}=$ENV{HOME}."/.qtc"; }
+	if ( ! $obj->{qtc}->{query} ) { $obj->{qtc}->{query}=qtc::query->new(path=>$obj->{qtc}->{path}); }
+
 }
+sub qtc_query { my $obj; return $obj->{qtc}->{query}; }
 
 sub mode_ask_call {
 	my $obj=shift; 
 
+}
+
+sub mode_show_messages {
+	my $obj=shift; 
+	my $q=$obj->query;
+	if ( ! $q->param("type") ) { $q->param("type", "new"); }
+	my $type=$q->param("type");
+	if ( $type !~ /^((all)|(new)|(sent))$/ ) { return "<h1>FAIL telegram type invalid</h1>"; }
+	if ( ! $q->param("call") ) { return "<h1>I don't have a call</h1>"; }
+
+	my @msgs=$obj->qtc_query->list_telegrams($call, $type);
 }
 
 ########################################################
