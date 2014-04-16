a006daa0b74c8fbc41080e48f5a9e9d47c383152
Hans Freitag <zem.nospam.20@fnordpol.de>
2014-04-16 09:21:08 +0200

    a bit more webapp stuff html elements generator

--- src/qtc/WebApp.pm
+++ src/qtc/WebApp.pm
@@ -17,9 +17,66 @@ sub setup {
 }
 sub qtc_query { my $obj; return $obj->{qtc}->{query}; }
 
+sub html_input_hidden {
+	my $obj=shift; 
+	my $r; 
+	foreach my $p (keys %{$obj->{qtc}->{exports}}) {
+		$r.=$obj->html_e("input", {
+				type=>"hidden", 
+				name=>$p,
+				value=>$obj->q->param($p),
+			}
+		); 
+	}
+	return $r; 
+}
+
+sub html_form {
+	my $obj=shift; 
+	my @r=@_; 
+	my $x=$obj->html_e("form", {
+		action=>$obj->q->url(-full=>1),
+		method=>"POST",
+		}, 
+		$obj->html_input_hidden,
+		@r,
+	); 
+	return $x; 
+}
+
+sub html_e {
+	my $obj=shift; 
+	my $name=shift; 
+	my $p=shift; 
+	my @r=@_; 
+	
+	print "<$name "; 
+	foreach my $key (keys %$p) { print "$key=\"".$obj->q->escapeHTML($$p{$key})."\" "; }
+	print ">"; 
+	print join("", @r);
+	print "</$name>";  
+}
+
+sub html_table {
+	my $obj=shift;
+	return $obj->html_e("table", @_); 
+}
+
+sub html_h1 {
+	my $obj=shift;
+	return $obj->html_e("h1", @_); 
+}
+
 sub mode_ask_call {
 	my $obj=shift; 
+	my $r;
 
+	$r.=$obj->html_form(
+		'<input type="text" size="10" maxlength="20" name="call"/>',
+		'<input type="submit" name="QTC?"/>',
+	); 
+
+	return $r; 
 }
 
 sub mode_show_messages {
@@ -31,6 +88,7 @@ sub mode_show_messages {
 	if ( ! $q->param("call") ) { return "<h1>I don't have a call</h1>"; }
 
 	my @msgs=$obj->qtc_query->list_telegrams($call, $type);
+	
 }
 
 ########################################################
