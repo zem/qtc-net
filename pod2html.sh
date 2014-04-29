f9e44e02ca7f9792db1b7d434b22f646ff982566
Hans Freitag <zem.nospam.20@fnordpol.de>
2014-04-29 23:57:23 +0200

    pod2html

---
+++ pod2html.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TARGET=www/pod
+rm -r $TARGET
+mkdir $TARGET
+cd src
+find qtc -name "*.pm" | (
+	while read file
+	do
+		modname=`echo ${file%.pm} | sed -e 's?/?::?g'` 
+		pod2html $file > ../${TARGET}/${modname}.html
+	done
+)
