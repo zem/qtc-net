1bbd4676a4ba205c9c248e1fbf2e5f3b4228c83f
Hans Freitag <zem.nospam.20@fnordpol.de>
2014-05-19 09:46:36 +0200

    qtc message dumper

---
+++ src/qtc_dump_msg.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use Data::Dumper; 
+use qtc::msg; 
+
+my $msg=qtc::msg->new(path=>$ARGV[0], filename=>$ARGV[1]); 
+
+$msg->has_valid_type; 
+$msg->is_object_valid; 
+
+print Dumper($msg); 
