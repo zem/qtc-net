8548baa98675b4eb1103c5ef5205db52c06be858
Hans Freitag <zem.nospam.20@fnordpol.de>
2014-07-08 00:42:05 +0200

    two direction sync including special path

--- bin/qtc_sync_http.pl
+++ bin/qtc_sync_http.pl
@@ -2,11 +2,18 @@
 
 use qtc::interface::http; 
 
-if ( $#ARGV == -1 ) { print "Usage: $0 url [url2] ...\n"; exit; }
+if ( $#ARGV == -1 ) { print "Usage: $0 [-d PATH_TO_QTC] url [url2] ...\n"; exit; }
+
+my $path=$ENV{HOME}."/.qtc"; 
+if ( $ARGV[0] eq "-d" ) {
+	shift(@ARGV); 
+	$path=shift(@ARGV); 
+}
 
 # url may be http://localhost/qtc_if.cgi
 foreach my $url (@ARGV) {
-	my $if=qtc::interface::http->new(url=>$url); 
+	my $if=qtc::interface::http->new(path=>$path, url=>$url); 
 	$if->sync("/out"); 
+	$if->sync_upload("/out"); 
 }
 
